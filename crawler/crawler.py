5ac384cf78bd1b83b81b531075dac73a945c19a9
braden624 <bradenjames12@icloud.com>
2024-10-16 16:38:24 -0500

    Added logging to the crawler.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -4,15 +4,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pyvirtualdisplay import Display
-import time
+from datetime import datetime
+import logging
+import os
 import random
 import string
+import time
+
+log_dir = 'crawler_logs'
+timestamp = datetime.now().strftime('%Y%m%d_%H%M')
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, f'{timestamp}_crawler.log'), level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def generate_random_string():
     letters_and_digits = string.ascii_letters + string.digits
     return ''.join(random.choice(letters_and_digits) for i in range(10))
 
 def register(username, password, driver):
+    start_time = time.time()
     driver.find_element(By.LINK_TEXT, 'Click to Register!').click()
     driver.find_element(By.ID, 'username').send_keys(username)
     driver.find_element(By.ID, 'email').send_keys(username + '@gmail.com')
@@ -20,16 +29,26 @@ def register(username, password, driver):
     driver.find_element(By.ID, 'password2').send_keys(password)
     driver.find_element(By.ID, 'submit').click()
     success = driver.find_element(By.CLASS_NAME, 'alert-info').text
-    print(success)
+    duration = time.time() - start_time
+    if (success == 'Congratulations, you are now a registered user!'):
+        logging.info(f"Registration successful for user '{username}': (Duration {duration:.5f} s)")
+    else:
+        logging.error(f"Registration failed for user '{username}': (Duration {duration:.5f} s)")
 
 def login(username, password, driver):
+    start_time = time.time()
     driver.find_element(By.ID, 'username').send_keys(username)
     driver.find_element(By.ID, 'password').send_keys(password)
     driver.find_element(By.ID, 'submit').click()
     success = driver.find_element(By.TAG_NAME, 'h1').text
-    print(success)
+    duration = time.time() - start_time
+    if (success == 'Hi, ' + username + '!'):
+        logging.info(f"Login successful for user '{username}': (Duration {duration:.5f} s)")
+    else:
+        logging.error(f"Login failed for user '{username}': (Duration {duration:.5f} s)")
 
-def logout(driver):
+def logout(username, driver):
+    start_time = time.time()
     driver.find_element(By.CLASS_NAME, 'navbar-toggler-icon').click()
     try:
         logout_link = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.LINK_TEXT, 'Logout')))
@@ -37,23 +56,36 @@ def logout(driver):
     except Exception as e:
         print(e)
     success = driver.find_element(By.CLASS_NAME, 'alert-info').text
-    print(success)
+    duration = time.time() - start_time
+    if (success == 'Please log in to access this page.'):
+        logging.info(f"Logout successful for '{username}': (Duration {duration:.5f} s)")
+    else:
+        logging.error(f"Logout failed for '{username}': (Duration {duration:.5f} s)")
 
 def forgot_password(email, driver):
+    start_time = time.time()
     driver.find_element(By.LINK_TEXT, 'Click to Reset It').click()
     driver.find_element(By.ID, 'email').send_keys(email)
     driver.find_element(By.ID, 'submit').click()
     success = driver.find_element(By.CLASS_NAME, 'alert-info').text
-    print(success)
+    duration = time.time() - start_time
+    if (success == 'Check your email for the instructions to reset your password'):
+        logging.info(f"Forgot password request sent for email '{email}': (Duration {duration:.5f} s)")
+    else:
+        logging.error(f"Forgot password request failed for email '{email}': (Duration {duration:.5f} s)")
 
-def post(message, driver):
+def post(username, message, driver):
+    start_time = time.time()
     driver.find_element(By.ID, 'post').send_keys(message)
     driver.find_element(By.ID, 'submit').click()
     posts = driver.find_elements(By.TAG_NAME, 'span')
     for post in posts:
         if post.text == message:
-            print('Posted: ' + message)
-            break
+            duration = time.time() - start_time
+            logging.info(f"'{username}' submitted a post successfully: (Duration {duration:.5f} s)")
+            return
+    duration = time.time() - start_time
+    logging.info(f"'{username}' post failed: (Duration {duration:.5f} s)")
 
 def main():
     # display = Display(visible=0, size=(800,600))
@@ -66,8 +98,8 @@ def main():
     message = generate_random_string()
     register(username=username, password=password, driver=driver)
     login(username=username, password=password, driver=driver)
-    post(message=message, driver=driver)
-    logout(driver=driver)
+    post(username=username, message=message, driver=driver)
+    logout(username=username, driver=driver)
     forgot_password(email=email, driver=driver)
     driver.quit()
 

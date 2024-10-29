06058ed94b241e01f20fbf22791ac7a235b042c7
Aishwarya Peri <aishwarya.peri-1@ou.edu>
2024-10-29 18:11:39 +0000

    Update __init__.py

--- microblog/app/api/__init__.py
+++ microblog/app/api/__init__.py
@@ -1,5 +1,21 @@
+import logging
 from flask import Blueprint
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
 
 bp = Blueprint('api', __name__)
 
+
 from app.api import users, errors, tokens
+
+logger.info("API Blueprint 'api' has been created and registered.")
+

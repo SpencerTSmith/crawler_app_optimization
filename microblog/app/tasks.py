111f60eabb7230eb0716f11c341056f83e661b99
Aishwarya Peri <aishwarya.peri-1@ou.edu>
2024-10-29 14:23:29 +0000

    Update tasks.py

--- microblog/app/tasks.py
+++ microblog/app/tasks.py
@@ -2,15 +2,27 @@ import json
 import sys
 import time
 import sqlalchemy as sa
+import logging
 from flask import render_template
 from rq import get_current_job
 from app import create_app, db
 from app.models import User, Post, Task
 from app.email import send_email
 
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+
+
+logger = logging.getLogger(__name__)
+
 app = create_app()
 app.app_context().push()
-
 
 def _set_task_progress(progress):
     job = get_current_job()
@@ -23,24 +35,28 @@ def _set_task_progress(progress):
         if progress >= 100:
             task.complete = True
         db.session.commit()
-
+        logger.info(f'Task progress updated to {progress}% for job ID {job.get_id()}.')
 
 def export_posts(user_id):
     try:
         user = db.session.get(User, user_id)
+        logger.info(f'Starting export of posts for user: {user.username} (User ID: {user_id})')
         _set_task_progress(0)
         data = []
         i = 0
         total_posts = db.session.scalar(sa.select(sa.func.count()).select_from(
             user.posts.select().subquery()))
+        logger.info(f'Total posts to export: {total_posts}')
+
         for post in db.session.scalars(user.posts.select().order_by(
                 Post.timestamp.asc())):
             data.append({'body': post.body,
                          'timestamp': post.timestamp.isoformat() + 'Z'})
-            time.sleep(5)
+            time.sleep(5)  # Simulating a long-running process
             i += 1
             _set_task_progress(100 * i // total_posts)
 
+        logger.info(f'Exporting {len(data)} posts to email.')
         send_email(
             '[Microblog] Your blog posts',
             sender=app.config['ADMINS'][0], recipients=[user.email],
@@ -49,8 +65,13 @@ def export_posts(user_id):
             attachments=[('posts.json', 'application/json',
                           json.dumps({'posts': data}, indent=4))],
             sync=True)
-    except Exception:
+        logger.info('Email sent successfully with exported posts.')
+
+    except Exception as e:
         _set_task_progress(100)
-        app.logger.error('Unhandled exception', exc_info=sys.exc_info())
+        logger.error('Unhandled exception occurred during post export', exc_info=sys.exc_info())
     finally:
         _set_task_progress(100)
+        logger.info('Export posts task completed.')
+
+

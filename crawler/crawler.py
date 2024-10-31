b3429e02fdef6d97fe9d03cbc088a69c4d476994
SpencerTSmith <spencertsmith2003@gmail.com>
2024-10-31 12:31:01 -0500

    add crawler benchmarking for user registration

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -19,7 +19,7 @@ os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename=os.path.join(log_dir, f'{timestamp}_crawler.log'), level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Args(StrEnum):
-    HEADLESS = "-h"
+    HEADLESS = "--headless"
     REGISTER = "--register"
     POST = "--post"
 
@@ -68,13 +68,51 @@ def register(username, password, driver):
         logging.error(f"Registration failed for user '{username}': (Duration {duration:.5f} s)")
 
 def register_bench():
-    # spin up processes
+    start_time = time.time()
+    workers = mp.Pool()
+    workers.map(register_worker, range(mp.cpu_count()))
+    workers.close()
+    end_time = time.time()
 
-    # each process gets a driver
+    total_duration = end_time - start_time
+
+    # each process should be registering 100 users
+    n_registered = mp.cpu_count() * 5
     
-    # each process goes to loop of registering users, counting the # it got to
+    logging.info(f"{n_registered} registrations complete in {total_duration}")
 
-    # add #'s together, divide by time taken
+def register_worker(_):
+    # each process gets a driver
+    driver_opts = Options()
+    driver_opts.add_argument("--window-size=1920,1080")
+    driver_opts.add_argument('--no-sandbox')
+    driver_opts.add_argument('--disable-dev-shm-usage')
+    driver_opts.add_argument("--headless=new")
+
+    driver = webdriver.Chrome(options=driver_opts)
+    driver.get("http://127.0.0.1:5000")
+
+    # each process registers 100 users
+    start_time = time.time()
+    for _ in range(5):
+        username = generate_random_string()
+        email = username + '@gmail.com'
+        password = generate_random_string()
+        driver.find_element(By.LINK_TEXT, 'Click to Register!').click()
+        driver.find_element(By.ID, 'username').send_keys(username)
+        driver.find_element(By.ID, 'email').send_keys(email)
+        driver.find_element(By.ID, 'password').send_keys(password)
+        driver.find_element(By.ID, 'password2').send_keys(password)
+        driver.find_element(By.ID, 'submit').click()
+        success = driver.find_element(By.CLASS_NAME, 'alert-info').text
+        end_time = time.time()
+        duration = end_time - start_time
+        if (success == 'Congratulations, you are now a registered user!'):
+            logging.info(f"Registration successful for user '{username}': (Duration {duration:.5f} s)")
+        else:
+            logging.error(f"Registration failed for user '{username}': (Duration {duration:.5f} s)")
+
+    driver.quit()
 
 def login(username, password, driver):
     start_time = time.time()
@@ -131,8 +169,6 @@ def post(username, message, driver):
     duration = time.time() - start_time
     logging.info(f"'{username}' post failed: (Duration {duration:.5f} s)")
 
-def private_message(username, message, driver):
-    return
 
 def main():
     # display = Display(visible=0, size=(800,600))
